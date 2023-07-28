1fd0105c6369530e3c61dc3fd2d4bfbae7f3e87b
dasha-musienko <dasha04.musienko@gmail.com>
2023-07-28 15:42:28 +0300

    Adds new updates in task5, task7

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,13 @@
 const inputFieldEl = document.querySelector("#name-input")
 const outputStringEl = document.querySelector("#name-output")
+const startValue = outputStringEl.textContent;
 
 inputFieldEl.addEventListener("input", inputFieldHandler)
 
 function inputFieldHandler(event) {
-  outputStringEl.textContent = event.currentTarget.value.trim()
+  if (event.currentTarget.value.trim() === "") {
+    outputStringEl.textContent = startValue;
+  } else {
+    outputStringEl.textContent = event.currentTarget.value.trim()
+  }
 }
--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const rangeEl = document.querySelector("#font-size-control")
 const textEl = document.querySelector("#text")
+textEl.style.fontSize = rangeEl.value + "px"
 
 rangeEl.addEventListener("input", inputRangeHandler)
 
